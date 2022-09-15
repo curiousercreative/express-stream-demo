9b34986a12a159a08701520477f22264438c9b44
Winston Hoy <winston@commonenergy.us>
2022-09-15 13:07:31 -0400

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.sendStatus(204));
+app.post('/', (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+
+  let progress = 0;
+
+  // stream file upload progress back to client
+  req.on('data', chunk => {
+    progress += chunk.length;
+    res.write(`\n${progress}`);
+  });
+
+  req.on('end', () => res.end());
+});
+
+const server = app.listen(3333, () => {
+  console.info(`Express running → PORT ${server.address().port}`);
+});
